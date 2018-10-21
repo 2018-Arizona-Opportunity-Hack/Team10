2b031f9dc9f91dc6b983c4e25cc04d0994a4b03e
Daniar T <moonyguypie@gmail.com>
2018-10-21 11:10:31 -0700

    last ui fix

--- signup/main.js
+++ signup/main.js
@@ -1,21 +1,49 @@
 const express = require('express');
 const path = require('path');
 const history = require('connect-history-api-fallback');
+const { Pool } = require('pg');
 
 const app = express();
 
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+const staticFileMiddleware = express.static(path.join(__dirname));
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 app.use(staticFileMiddleware);
 app.use(history({
   disableDotRule: true,
   verbose: true
 }));
+
 app.use(staticFileMiddleware);
 
 app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'));
+  res.render(path.join('index.html'));
 });
+
+app.get('/admin', function (req, res) {
+  res.render(path.join('admin.html'));
+});
+
+app.get('/mycheckin', function (req, res) {
+  res.render(path.join('mycheckin.html'));
+});
+
+// app.get('/db', async (req, res) => {
+//     try {
+//       const client = await pool.connect()
+//       const result = await client.query('SELECT * FROM test_table');
+//       const results = { 'results': (result) ? result.rows : null};
+//       res.render('pages/db', results );
+//       client.release();
+//     } catch (err) {
+//       console.error(err);
+//       res.send("Error " + err);
+//     }
+//   });
 
 var server = app.listen(process.env.PORT || 8080, function () {
   var port = server.address().port;
